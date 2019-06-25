26b5396165e248884cd352073c64cc77329c27c7
Connie Chen <Chen_Connie@bah.com>
2019-06-25 10:19:03 -0700

    added card components

--- client/app/scripts/components/dashboard.js
+++ client/app/scripts/components/dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardText, CardBody,
+import { Card, CardText,
   CardTitle } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,12 +14,22 @@ export default class Dashboard extends React.Component {
             <div className="dash-status">Everything looks good!</div>
             <div className="pie-charts">Pie charts go here</div>
           </div>
-          <div>
-            <Card>
-              <CardBody>
-                <CardTitle>TITLE</CardTitle>
-                <CardText>Text</CardText>
-              </CardBody>
+          <div className="bottom">
+            <Card className="card">
+              <CardTitle>3</CardTitle>
+              <CardText>nodes</CardText>
+            </Card>
+            <Card className="card">
+              <CardTitle>105</CardTitle>
+              <CardText>pods</CardText>
+            </Card>
+            <Card className="card">
+              <CardTitle>42</CardTitle>
+              <CardText>containers</CardText>
+            </Card>
+            <Card className="card">
+              <CardTitle>31</CardTitle>
+              <CardText>services</CardText>
             </Card>
           </div>
         </div>
