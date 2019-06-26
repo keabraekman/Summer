e457aef340001c309b79bfe49793876e39f43ec1
Connie Chen <Chen_Connie@bah.com>
2019-06-26 11:45:45 -0700

    test if getting real cpu/memory info works

--- client/app/scripts/components/dashboard.js
+++ client/app/scripts/components/dashboard.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import { Card, CardText,
   CardTitle } from 'reactstrap';
+import { connect } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { shownNodesSelector } from '../selectors/node-filters';
 
-// TODO Connect to actions
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
+  getCPU = (node) => {
+    const values = {};
+    ['metrics', 'metadata'].forEach((collection) => {
+      if (node[collection]) {
+        node[collection].forEach((field) => {
+          const result = Object.assign({}, field);
+          result.valueType = collection;
+          values[field.id] = result;
+        });
+      }
+    });
+    return values.docker_cpu_total_usage.value;
+  }
+
+  getMemory = (node) => {
+    const values = {};
+    ['metrics', 'metadata'].forEach((collection) => {
+      if (node[collection]) {
+        node[collection].forEach((field) => {
+          const result = Object.assign({}, field);
+          result.valueType = collection;
+          values[field.id] = result;
+        });
+      }
+    });
+    return values.docker_memory_usage.value;
+  }
+
   render() {
+    const { nodes } = this.props;
+    const cpu = nodes.reduce((total, node) => total + this.getCPU(node));
+    const memory = nodes.reduce((total, node) => total + this.getMemory(node));
     return (
       <div className="dashboard">
         <h1>Dashboard</h1>
         <div className="view">
           <div className="top">
             <div className="dash-status">Everything looks good!</div>
-            <div className="pie-charts">Pie charts go here</div>
+            <div className="pie-charts">
+              {cpu}% CPU used
+              {memory}MB Memory used
+            </div>
           </div>
           <div className="bottom">
             <Card className="card">
@@ -37,3 +72,11 @@ export default class Dashboard extends React.Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    nodes: shownNodesSelector(state)
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
