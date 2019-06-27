43642fc057edb9c685328532bdb399043bb58762
Bryan Pan <604985@bah.com>
2019-06-27 12:58:56 -0700

    error bar mvp

--- client/app/scripts/components/error-bar.js
+++ client/app/scripts/components/error-bar.js
@@ -5,28 +5,46 @@ import { Icon } from 'react-icons-kit';
 import { connect } from 'react-redux';
 import { shownNodesSelector } from '../selectors/node-filters';
 
+
 export const ErrorIcon = () => <Icon icon={warning} />;
 
 export class ErrorBar extends React.Component {
+  formatData() {
+    var data1 = this.props.nodes.toList().toJS();
+    var return_data = [];
+    var state;
+    for(var i = 0; i < data1.length; i++)
+    {
+      if(this.props.topologies === "pods"){
+        state = data1[i]['metadata'][0]['value'];
+        if(data1[i]['metadata'][0]['id'] === "kubernetes_state" && state === "Running"){
+          return_data[i] = {name: data1[i]['rank'], state: state}
+        }
+      }
+    }
+    return (
+      return_data
+    );
+  }
   render() {
+    var data = this.formatData();
     return (
       <ListGroup className='err-bar'>
-        <ListGroupItem className="err-item" href="#"><ErrorIcon />  Pod 1.12.9 ImagePullBackOff</ListGroupItem>
-        <ListGroupItem className="err-item" href="#"><ErrorIcon /> Pod 2.83.3 Excessive Restarts</ListGroupItem>
-        <ListGroupItem className="err-item" tag="a" href="#"><ErrorIcon /> Morbi leo risus</ListGroupItem>
-        <ListGroupItem className="err-item" tag="a" href="#"><ErrorIcon /> Porta ac consectetur ac</ListGroupItem>
-        <ListGroupItem className="err-item" tag="a" href="#"><ErrorIcon /> Vestibulum at eros</ListGroupItem>
+        {data.map((element) => 
+        <ListGroupItem className="err-item" href="#"><ErrorIcon /> {element.name.slice(0,15)}... {element.state}</ListGroupItem>
+        )}
       </ListGroup>
     );
   }
 }
 
 function mapStatetoProps(state){
-	return {
-		nodes: shownNodesSelector(state)
-	};
+  return {
+    nodes: shownNodesSelector(state),
+    topologies: state.get('currentTopologyId')
+  };
 }
 
 export default connect(
-	 mapStatetoProps
+  mapStatetoProps
 )(ErrorBar);
