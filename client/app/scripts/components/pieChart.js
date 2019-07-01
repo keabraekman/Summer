f145e43881e9c51df0187587263abb9d6cd43813
Nagarajan <604980@bah.com>
2019-07-01 13:52:20 -0700

    Added pie chart

---
+++ client/app/scripts/components/pieChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+const num = 56; 
+
+class PieChart extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { data: [], layout: {}, frames: [], config: {} };
+  }
+
+  render() {
+    return (
+
+      <Plot
+        data={
+        [
+          {
+          values: this.props.values,
+          labels: ['Used', 'Available'],
+          type: 'pie',
+          }
+        ]
+       }
+       
+        layout = {
+          {height: 280, width: 300,
+          title: this.props.title}
+        }
+      />
+
+    );
+  }
+}
+
+export default PieChart;
