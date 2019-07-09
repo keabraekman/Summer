11eff84f5e97ef1021080147823d69e6977f011b
Nagarajan <604980@bah.com>
2019-07-09 15:25:31 -0700

    Filter modal broken

--- client/app/scripts/components/sidebar.js
+++ client/app/scripts/components/sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Sidebar({children, classNames}) {
   const className = `tour-step-anchor sidebar ${classNames}`;
   return (
-    <div className={className} hidden>
+    <div className={className}>
       {children}
     </div>
   );
