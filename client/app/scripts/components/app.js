3788f32e4d732e2b9a30cf4b13e97cfcca2a445c
kea <kea.braekman@mail.com>
2019-07-24 11:44:11 -0700

    placed breadcrumb in jumbotron

--- client/app/scripts/components/app.js
+++ client/app/scripts/components/app.js
@@ -249,7 +249,7 @@ class App extends React.Component {
             </div>
           </div>
 
-          <Breadcrumb />
+          {/* <Breadcrumb /> */}
           {/* <NodeDetailsRelatives /> */}
           {/* <NodeDetailsRelativesLink /> */}
 
--- client/app/scripts/components/nodes-jumbotron.js
+++ client/app/scripts/components/nodes-jumbotron.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NodesChart from '../charts/nodes-chart';
+import Breadcrumb from './breadcrumb'
 
 class NodesJumbotron extends React.Component {
     render() {
     return (
       <div id="NodeJumbotron">
-        <h5>Host: Minikube / Pod: AshPod / Container: AshContainer / </h5>
+        {/* <h5>Host: Minikube / Pod: AshPod / Container: AshContainer / </h5> */}
+        <Breadcrumb />
         <NodesChart/>
       </div>
     );
